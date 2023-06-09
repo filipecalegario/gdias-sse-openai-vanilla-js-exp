034457246926cdadc3b9753032160bc8b0e4abe3
filipecalegario <fcac@cin.ufpe.br>
2023-06-09 10:15:48 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+async function generate() {
+  var text = document.getElementById("myTextarea").value;
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: promptInput.value }],
+        stream: true
+      }),
+    });
+
+    // Read the response as a stream of data
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder("utf-8");
+    resultText.innerText = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        break;
+      }
+      // Massage and parse the chunk of data
+      const chunk = decoder.decode(value);
+      const lines = chunk.split("\\n");
+      const parsedLines = lines
+        .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
+        .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
+        .map((line) => JSON.parse(line)); // Parse the JSON string
+
+      for (const parsedLine of parsedLines) {
+        const { choices } = parsedLine;
+        const { delta } = choices[0];
+        const { content } = delta;
+        // Update the UI with the new content
+        if (content) {
+          resultText.innerText += content;
+        }
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+
+
+
+
+
+
+
+
+  document.getElementById("generatedContent").innerHTML = text;
+}
